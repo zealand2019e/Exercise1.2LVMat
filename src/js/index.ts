5dfad17fd5dbb0ddb4d128bcafbf9a743da48b3d
Matúš Kalanin <mato.kalanin@gmail.com>
2019-10-20 02:55:13 +0200

    Release

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,63 @@
-interface Person {
-    firstName: string;
-    lastName: string;
-}
+let contenElement: HTMLDivElement = <HTMLDivElement>document.getElementById("content");
+let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById("myInput");
+let buttonElement: HTMLButtonElement = <HTMLButtonElement>document.getElementById("myButton");
+let selectElement: HTMLSelectElement = <HTMLSelectElement>document.getElementById("options");
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+buttonElement.addEventListener("click",
+    (getChildrensChildren) => {
+        var para = document.createElement("li");
+        var element = document.getElementById("newLine");
+        var numberOfChildren = element.childElementCount + 1;
+        var setNumberOfChildren = numberOfChildren;
+        
+        switch (selectElement.value) {
+            case "UpperCase":
+                {
+                    var xyz = inputElement.value.toUpperCase();
+                    break;
+                }
+            
+            case "LowerCase":
+                {
+                    var xyz = inputElement.value.toLowerCase();
+                    break;
+                }
+        }
+        if(xyz.length != 0)
+        {
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+               para.innerHTML = xyz;
+               var dsad= "" + numberOfChildren++;   
+               var y = document.getElementById("newLine").append(dsad, para ); 
+            //   document.getElementById("newLine").append(dsad);
+
+            }
+        else
+        {
+            window.alert("You must write something");
+        }
+       
+         
+        console.log(dsad)
+        /*
+       for (let i = 0; i <= numberOfChildren;  i++) 
+        {
+            if(para.childNodes[i].nodeType != 3){
+                if(getChildrensChildren)
+                    id += getCount(para.childNodes[i],true);
+                id++;
+            }
+            
+    
+        }
+        
+        */       
+
+        
+    });
+
+
+
+
+
+
